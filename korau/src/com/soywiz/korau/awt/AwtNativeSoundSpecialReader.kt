29cdb08fac831c943e9fa0285ff3c3619d6bcab0
Carlos Ballesteros Velasco <soywiz@gmail.com>
2017-04-26 22:52:33 +0200

    AWT must decode data

--- korau/src/com/soywiz/korau/awt/AwtNativeSoundSpecialReader.kt
+++ korau/src/com/soywiz/korau/awt/AwtNativeSoundSpecialReader.kt
@@ -1,5 +1,7 @@
 package com.soywiz.korau.awt
 
+import com.soywiz.korau.format.readAudioData
+import com.soywiz.korau.format.toNativeSound
 import com.soywiz.korau.sound.NativeSound
 import com.soywiz.korio.vfs.LocalVfs
 import com.soywiz.korio.vfs.Vfs
@@ -7,7 +9,7 @@ import com.soywiz.korio.vfs.VfsSpecialReader
 
 class AwtNativeSoundSpecialReader : VfsSpecialReader<NativeSound>(NativeSound::class.java) {
 	suspend override fun readSpecial(vfs: Vfs, path: String): NativeSound = when (vfs) {
-		is LocalVfs -> AwtNativeSound(vfs[path].readBytes())
-		else -> AwtNativeSound(vfs[path].readBytes())
+		is LocalVfs -> vfs[path].readAudioData().toNativeSound()
+		else -> vfs[path].readAudioData().toNativeSound()
 	}
 }
