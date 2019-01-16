4e5da114072fd8a9bd444bada3415e7fdd31845e
soywiz <soywiz@gmail.com>
2019-01-16 14:08:10 +0100

    Try to fix downloading on travis-windows

--- travis_win.bat
+++ travis_win.bat
@@ -1,6 +1,6 @@
 REM free some space
 
-POWERSHELL "(New-Object System.Net.WebClient).DownloadFile('https://github.com/ojdkbuild/ojdkbuild/releases/download/1.8.0.191-1/java-1.8.0-openjdk-1.8.0.191-1.b12.ojdkbuild.windows.x86_64.zip','java.zip')"
+POWERSHELL "[Net.ServicePointManager]::SecurityProtocol = 'tls12, tls11, tls'; (New-Object System.Net.WebClient).DownloadFile('https://github.com/ojdkbuild/ojdkbuild/releases/download/1.8.0.191-1/java-1.8.0-openjdk-1.8.0.191-1.b12.ojdkbuild.windows.x86_64.zip','java.zip')"
 POWERSHELL "Expand-Archive java.zip -DestinationPath c:\java8"
 DIR c:\java8
 SET JAVA_HOME=c:\java8
